cab7cda9836cbb71344b6c98cdb26f758cbe284a
Ejap23 <146755642+Ejap23@users.noreply.github.com>
2024-03-02 15:51:24 +0800

    forms

---
+++ src/app/forms/forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css']
+})
+export class FormsComponent {
+
+}
