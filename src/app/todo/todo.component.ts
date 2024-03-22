64eeafd5eb2693997f769bf08932b5762f2e11a6
Ejap23 <146755642+Ejap23@users.noreply.github.com>
2024-03-22 22:29:41 +0800

    exam

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
   standalone:true,
-  imports:[ReactiveFormsModule],
+  imports:[ReactiveFormsModule, NgFor],
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
@@ -16,6 +17,10 @@ export class TodoComponent {
 
     this.todoList.push(this.todoItem.value as string)
     this .todoItem.setValue("")
+  }
+
+  removeItem(index: number) {
+    this.todoList.splice(index, 1);
   }
 
 }
