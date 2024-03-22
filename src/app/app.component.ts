0976de1fa0a4b1cb7c16b5c91c03425e5a92c707
Ejap23 <146755642+Ejap23@users.noreply.github.com>
2024-03-22 17:08:24 +0800

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 @Component({ 
   selector: 'app-root', 
-  standalone: true, 
-  imports: [RouterOutlet], 
-  templateUrl: './app.component.html', 
-  styleUrl: './app.component.css'})
+  standalone: true,
+  imports:[RouterOutlet],
+  templateUrl:'./app.component.html',
+  styleUrl: './app.component.css'
+})
 export class AppComponent { title = 'angular-test';}
